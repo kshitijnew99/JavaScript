9cc3a7f467c202e778624c2e6295ee2c390658a9
kshitij99 <kshitijraoranjannew99@gmail.com>
2025-01-17 00:38:14 +0530

    random commmit

--- basics/basic3.js
+++ basics/basic3.js
@@ -11,4 +11,5 @@ console.log(value)
 if score = null before type object and it's number conversion is  0 with the type number.
 if score = undefined before type undefined it's number conversion is  NaN with the type number.
 ture is converted into 1 and vica versa
-*/
+
+
