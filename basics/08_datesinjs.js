d1a1277a1b3ab55f8a017f17a5b52bedb191f3d4
kshitij99 <kshitijraoranjannew99@gmail.com>
2025-01-26 22:52:02 +0530

    dates file added

---
+++ basics/08_datesinjs.js
@@ -0,0 +1,33 @@
+// Date 
+
+let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+
+// console.log(typeof myDate)
+
+let myCurrentDate = new Date(2025 , 11 , 26)// months in JS are starts from 0
+
+// console.log(myCurrentDate.toDateString())
+
+let myCurrentDates = new Date(2025 , 11 , 26, 16 ,30 )
+
+let myCurrentDatez = new Date("2025-01-27")
+
+// console.log(myCurrentDatez.toLocaleString())
+
+let timeStamp = Date.now()
+
+console.log(timeStamp)
+console.log(myCurrentDatez.getTime())
+console.log(Math.floor(Date.now()/1000))
+
+let newdate = new Date()
+console.log(newdate.getTime())
+
+
+
+
+
+
--- basics/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-const newname = "      kshitijraoranjan      "
-
-// console.log(newname)
-// console.log(newname.trimStart())
-
-// const url = "https://kshitij.99/mr.zeus"
