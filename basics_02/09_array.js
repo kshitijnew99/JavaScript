1301bfd949a691f5e125754659e859b2702bf533
kshitij99 <kshitijraoranjannew99@gmail.com>
2025-01-27 19:58:56 +0530

    array file

---
+++ basics_02/09_array.js
@@ -0,0 +1,14 @@
+let array =  new Array(1,2,3,4,5,6,7)
+console.log(array)
+
+// Array Method
+
+array.push(8)
+console.log(array)
+array.pop()
+console.log(array)
+array.unshift(10)
+console.log(array)
+array.shift()
+console.log(array)
+console.log(array.includes(9))
