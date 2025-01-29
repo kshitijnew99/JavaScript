ee7176d1340e72f5984a3081d150d1103a1be82c
kshitij99 <kshitijraoranjannew99@gmail.com>
2025-01-30 02:31:34 +0530

    Array file added

--- basics_02/09_array.js
+++ basics_02/09_array.js
@@ -1,14 +1,34 @@
 let array =  new Array(1,2,3,4,5,6,7)
-console.log(array)
+// console.log(array)
 
 // Array Method
 
-array.push(8)
-console.log(array)
-array.pop()
-console.log(array)
-array.unshift(10)
-console.log(array)
-array.shift()
-console.log(array)
-console.log(array.includes(9))
+// array.push(8)
+// console.log(array)
+// array.pop()
+// console.log(array)
+// array.unshift(10)
+// console.log(array)
+// array.shift()
+// console.log(array)
+// console.log(array.includes(9))
+// console.log(array.indexOf(3))
+
+// const newarr = array.join() // converts the array into the string
+
+// console.log(array)
+// console.log(newarr)
+// console.log(typeof newarr)
+
+console.log("A " , array)
+
+let new1 = array.slice(1,3)// only show the element of the array
+
+console.log(new1)
+console.log("B ",array)
+
+let new2 = array.splice(1,3)// Changes the real array
+
+console.log(new2)
+
+console.log("C", array)
---
+++ basics_02/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+const newarr = array.join()
+
+// console.log(array)
+// console.log(typeof newarr)
